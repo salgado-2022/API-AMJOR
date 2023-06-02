0e3bc9adebff57c3e8456b21c214b6b367e995aa
unknown <brandonquintero2005@gmail.com>
2023-06-02 16:51:35 -0500

    pa que me de

--- controllers/Anchetas/editarAncheta.js
+++ controllers/Anchetas/editarAncheta.js
@@ -1,23 +1,40 @@
 const db = require("../../database/db");
-
+const upload = require("../../models/multerConfig");
 
 const listarEdAncheta = (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM ancheta WHERE ID_Ancheta = ?";
+  
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      return res.json({ Message: "Error en el servidor" });
+    }
+    return res.json(result);
+  });
+};
 
-    const sql = "SELECT * FROM ancheta WHERE ID_Ancheta = ?";
-    const id = req.params.id;
-    db.query(sql,[id], (err, result)=>{
-        if(err) return res.json({Message: "Error en el servidor "});
-        return res.json(result);
-    })
-}
+const editarAncheta = (upload.single('image'), (req, res) => {
+  const id = req.params.id;
+  const { NombreAncheta, Descripcion, PrecioUnitario, ID_Estado } = req.body;
+  
+  // Consulta para actualizar todos los campos excepto 'image'
+  const updateAnchetaSQL = "UPDATE ancheta SET `NombreAncheta`=?, `Descripcion`=?, `PrecioUnitario`=?, `ID_Estado`=? WHERE ID_Ancheta=?";
+  db.query(updateAnchetaSQL, [NombreAncheta, Descripcion, PrecioUnitario, ID_Estado, id], (err, result) => {
+    if (err) {
+      return res.json({ Message: "Error en el servidor" });
+    }
+    return res.json(result);
+  });
 
-const editarAncheta = (req, res) => {
-    const sql = "UPDATE ancheta SET `NombreAncheta`=?, `Descripcion`=?, `PrecioUnitario`=?, `ID_Estado`=? WHERE ID_Ancheta=?";
-    const id = req.params.id;
-    db.query(sql,[req.body.NombreAncheta, req.body.Descripcion, req.body.PrecioUnitario, req.body.ID_Estado, id], (err, result)=>{
-        if(err) return res.json({Message: "Error en el servidor "});
-        return res.json(result);
-    })
-}
+  // Consulta para actualizar solo el campo 'image' si se proporciona una nueva imagen
+  if (req.file) {
+    const updateImageSQL = "UPDATE ancheta SET `image`=? WHERE ID_Ancheta=?";
+    db.query(updateImageSQL, [req.file.filename, id], (err, result) => {
+      if (err) {
+        return res.json({ Message: "Error en el servidor" });
+      }
+    });
+  }
+});
 
-module.exports ={listarEdAncheta, editarAncheta}
+module.exports = { listarEdAncheta, editarAncheta };
--- controllers/Anchetas/eliminarAncheta.js
+++ controllers/Anchetas/eliminarAncheta.js
@@ -1,14 +1,35 @@
 const db = require("../../database/db");
+const fs = require("fs");
 
+const eliminarAncheta = (req, res) => {
+  const id = req.params.id;
+  
+  // Obtén la ruta de la imagen almacenada en la base de datos
+  const obtenerRutaImagenSQL = "SELECT image FROM ancheta WHERE ID_Ancheta = ?";
+  db.query(obtenerRutaImagenSQL, [id], (err, result) => {
+    if (err) {
+      return res.json({ Message: "Error en el servidor" });
+    }
 
-const eliminarAncheta = ( '/delete/:id', (req, res) => {
+    const rutaImagen = result[0].image; // Asegúrate de que la columna de la imagen se llame 'image' en tu tabla
 
-    const sql = "DELETE FROM ancheta WHERE ID_Ancheta =?";
-    const id = req.params.id;
-    db.query(sql, [id], (err, result)=>{
-        if(err) return res.json({Message: "Error en el servidor "});
-        return res.json(result);
-    })
-})
+    // Elimina la ancheta de la base de datos
+    const eliminarAnchetaSQL = "DELETE FROM ancheta WHERE ID_Ancheta = ?";
+    db.query(eliminarAnchetaSQL, [id], (err, result) => {
+      if (err) {
+        return res.json({ Message: "Error en el servidor" });
+      }
+      
+      // Elimina el archivo de imagen localmente
+      fs.unlink(`public/anchetas/${rutaImagen}`, (err) => {
+        if (err) {
+            return res.json({ Message: "Error al eliminar la imagen" });
+        }
+      });
 
-module.exports ={eliminarAncheta}
+      return res.json(result);
+    });
+  });
+};
+
+module.exports = { eliminarAncheta };
