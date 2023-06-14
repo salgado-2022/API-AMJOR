bb23c3c48bc17fb33ddd0bc2b99cd859a46e66e5
Salgado-2022 <salgadojuandavid419@gmail.com>
2023-06-14 18:19:28 -0500

    Ajuste en el controlador de crear ancheta

--- controllers/Anchetas/crearAncheta.js
+++ controllers/Anchetas/crearAncheta.js
@@ -1,25 +1,44 @@
 const db = require("../../database/db");
 const upload = require("../../models/multerConfig")
 
-const postAncheta = (upload.single('image') ,(req, res) =>{
+const postAncheta = (upload.single('image'), (req, res) => {
+    const sql = "INSERT INTO ancheta (`NombreAncheta`,`Descripcion`,`PrecioUnitario`,`image`) VALUES (?)";
+    const values = [
+        req.body.NombreAncheta,
+        req.body.Descripcion,
+        req.body.PrecioUnitario,
+        req.file.filename
+    ];
 
-        const sql ="INSERT INTO ancheta (`NombreAncheta`,`Descripcion`,`PrecioUnitario`,`image`) VALUES (?)";
-    
-        const values =[
-            req.body.NombreAncheta,
-            req.body.Descripcion,
-            req.body.PrecioUnitario,
-            req.file.filename
-        ]
-        
-        db.query(sql, [values] , (err, result ) => {
-            if (err) {
-                console.error(err);
-                return res.json({ Error: "Error inserting formdata" });
-            }
-            return res.json({Status: "Success"});
-    })
+    db.query(sql, [values], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.json({ Error: "Error inserting formdata" });
+        }
 
-})
+        const idAncheta = result.insertId;
 
-module.exports = {postAncheta}
+        const sql2 = "INSERT INTO insumos_ancheta (`ID_Ancheta`, `ID_Insumo`, `Cantidad`, `Precio`) VALUES (?,?,?,?)";
+        const insumos = JSON.parse(req.body.Insumos);
+
+        // Recorrer los insumos y ejecutar la inserción para cada uno
+        insumos.forEach((insumo) => {
+            const insumoValues = [
+                idAncheta,
+                insumo.idInsumo,
+                insumo.cantidad,
+                insumo.precio
+            ];
+            db.query(sql2, insumoValues, (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.json({ Error: "Error inserting insumos" });
+                }
+            });
+        });
+
+        return res.json({ Status: "Success" });
+    });
+});
+
+module.exports = { postAncheta }
--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,6 @@ const {postRegistro } = require('../controllers/Acceso/register');
 const {getInformacionDelCliente} = require('../controllers/prueba');
 const {authenticateToken} = require('../controllers/Acceso/authenticateToken');
 const {logout} = require('../controllers/Acceso/logout');
-const {listarPedidos} = require('../controllers/Pedido/listar');
 
 //USUARIOS
 const { postCrearUsuario} = require('../controllers/Usuarios/crearUsuario');
@@ -38,7 +37,6 @@ const {DetalleAncheta} = require('../controllers/Pedido/DetalleInsumo');
 //GET
 router.get('/prueba',authenticateToken, getInformacionDelCliente)
 router.get('/logout',logout)
-router.get('/admin/pedidos',listarPedidos)
 router.get('/admin/pedidos/detalle/:id',DetallePedido)
 router.get('/admin/pedidos/detalle/ancheta/:id',DetalleAncheta)
 router.get('/admin/anchetas',listarAncheta)
