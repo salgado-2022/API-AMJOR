3ecac09314d830614b22eaf65d090a34f259fb38
juandaxx2343 <juandathebass@gmail.com>
2023-07-14 12:29:54 -0500

    cambios para la modal

--- controllers/Usuarios/editarUsuario.js
+++ controllers/Usuarios/editarUsuario.js
@@ -1,10 +1,14 @@
 const db = require("../../database/db");
+const bcrypt = require("bcrypt");
 
 const listarEdUsuarios = (req, res) => {
   const sql = "SELECT * FROM usuario WHERE ID_Usuario = ?";
   const id = req.params.id;
   db.query(sql, [id], (err, result) => {
-    if (err) return res.json({ Message: "Error en el servidor" });
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ Message: "Error en el servidor" });
+    }
     return res.json(result);
   });
 };
@@ -13,16 +17,43 @@ const editarUsuario = (req, res) => {
   const sql = "UPDATE usuario SET correo = ?, contrasena = ? WHERE ID_Usuario = ?";
   const id = req.params.id;
   const nuevoCorreo = req.body.correo;
-  const nuevaContrasena = req.body.contrasena;
+  let nuevaContrasena = req.body.contrasena;
 
-  db.query(sql, [nuevoCorreo, nuevaContrasena, id], (err, result) => {
+  const obtenerContrasenaActual = "SELECT contrasena FROM usuario WHERE ID_Usuario = ?";
+  db.query(obtenerContrasenaActual, [id], (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ Message: "Error en el servidor" });
     }
 
-    return res.json(result);
+    const contrasenaActual = result[0].contrasena;
+
+    if (!nuevaContrasena) {
+      // Si no ingresa cambios para una nueva contraseña, mantener la contraseña actual
+      nuevaContrasena = contrasenaActual;
+      ejecutarActualizacion();
+    } else {
+      // Si se ingresa una nueva contraseña, encriptarla y luego realizar la actualización
+      bcrypt.hash(nuevaContrasena, 10, (err, hash) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ Message: "Error en el servidor" });
+        }
+        nuevaContrasena = hash;
+        ejecutarActualizacion();
+      });
+    }
   });
+
+  const ejecutarActualizacion = () => {
+    db.query(sql, [nuevoCorreo, nuevaContrasena, id], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ Message: "Error en el servidor" });
+      }
+      return res.json(result);
+    });
+  };
 };
 
 module.exports = { listarEdUsuarios, editarUsuario };
