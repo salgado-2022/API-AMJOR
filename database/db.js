b1f5c4c8e47b777757927eab9f24581b73f2c6df
salgado-2022 <salgadojuandavid419@gmail.com>
2023-08-13 14:09:07 -0500

    Configurando credenciales de la bd como variable de entorno

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+require('dotenv').config()
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    port: 3306,
+})
+
+module.exports = db
