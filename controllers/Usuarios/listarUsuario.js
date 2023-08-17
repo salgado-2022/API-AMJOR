9588d5a7bab43724b074c9a39cbc1dfc603b9649
juandaxx2343 <juandathebass@gmail.com>
2023-08-17 06:43:14 -0500

    cambios

--- controllers/Usuarios/listarUsuario.js
+++ controllers/Usuarios/listarUsuario.js
@@ -1,19 +1,22 @@
 const db = require("../../database/db");
 
 const listarUsuario = (req, res) => {
+  // Consulta SQL para obtener todos los usuarios con su rol, incluyendo activos e inactivos
   const sql = `
     SELECT U.idUsuario, U.correo, U.Estado, R.Nombre_Rol
     FROM usuario U
     LEFT JOIN rol R ON U.ID_Rol = R.ID_Rol
-    WHERE U.Estado = 1
+    ORDER BY U.idUsuario ASC
   `;
 
+  // Ejecuta la consulta en la base de datos
   db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
-      return res.json({ Message: "Error en el servidor" });
+      return res.status(500).json({ Message: "Error en el servidor" });
     }
 
+    // Devuelve los resultados como respuesta JSON
     return res.json(result);
   });
 };
