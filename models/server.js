96438c34bc30787244796d4cf21fcb2fef87967b
Juansitoh <juanfergamer77@gmail.com>
2023-08-17 19:34:47 -0500

    Cambios para redirect a amjor.shop

--- models/server.js
+++ models/server.js
@@ -35,7 +35,7 @@ class Server {
 
     middlewares() {
         this.app.use(cors({
-            origin: ["http://localhost:3000","http://localhost:3001","https://heartfelt-bavarois-a44de2.netlify.app", "https://main--silver-muffin-e46c6e.netlify.app"],
+            origin: ["https://amjor.shop","https://www.amjor.shop","https://dashboard.amjor.shop","http://34.173.120.70","http://34.173.120.70:80","http://34.173.120.70:3000","http://34.173.120.70:3001","http://localhost:3000","http://localhost:3001"],
             methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
             credentials: true
         }));
