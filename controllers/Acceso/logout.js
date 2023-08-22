1cc5a3951fa2794d2a88bd1e2deb96fb811a4f5c
salgado-2022 <salgadojuandavid419@gmail.com>
2023-08-22 17:47:19 -0500

    validacion de correo y documento en el input

--- controllers/Acceso/logout.js
+++ controllers/Acceso/logout.js
@@ -1,11 +1,11 @@
 const logout = (req, res) => {
     // Configura los mismos atributos que utilizaste al crear la cookie
     const cookieOptions = {
-        path: '/',       // Ruta original donde se configuró la cookie
-        domain: '.amjor.shop', // Dominio original de la cookie
-        secure: true,    // Si la cookie se configuró como segura (HTTPS)
-        httpOnly: false,  // Si la cookie se configuró como HttpOnly
-        sameSite: 'None' // Valor original de SameSite
+        // path: '/',       // Ruta original donde se configuró la cookie
+        // domain: '.amjor.shop', // Dominio original de la cookie
+        // secure: true,    // Si la cookie se configuró como segura (HTTPS)
+        // httpOnly: false,  // Si la cookie se configuró como HttpOnly
+        // sameSite: 'None' // Valor original de SameSite
     };
 
     // Elimina la cookie configurando una fecha de expiración en el pasado
