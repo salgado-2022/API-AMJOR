77329de92def1f0f06fd90ee045bcf2a73250c72
salgado-2022 <salgadojuandavid419@gmail.com>
2023-08-22 21:05:11 -0500

    Registrar del login funcionando correctamente

--- controllers/Acceso/register.js
+++ controllers/Acceso/register.js
@@ -2,23 +2,42 @@ const db = require("../../database/db");
 const bcrypt = require("bcrypt");
 const salt = 10;
 
-const postRegistro = (req , res) =>{
-    const sql ="INSERT INTO usuario (`ID_Rol`,`correo`,`contrasena`) VALUES (?)";
-    bcrypt.hash(req.body.password.toString(), salt, (err, hash) =>{
-        if(err) return res.json({Error: "Error for hassing password"});
-        
-        const values =[
-            req.body.Rol = 1,
-            req.body.email,
-            hash
-        ]
-        db.query(sql, [values] , (err, result ) => {
-            if(err) return res.json({Error: " Inserting data error in server "});
-            return res.json({Status: "Success"});
 
-        })
-    })
+const postRegistro = (req, res) => {
 
-}
+    const { Documento, Nombre, Apellidos, Telefono, Email, Password } = req.body
 
-module.exports = {postRegistro}
+    console.log(Password)
+    console.log(Email)
+
+
+    bcrypt.hash(Password.toString(), salt, (err, hash) => {
+
+        if (err) return res.json({ Error: "Error en la contraseña" });
+
+        const userValues = [Email, hash];
+        console.log(hash)
+
+        const sql = "INSERT INTO usuario  (`correo`, `contrasena`) VALUES (?)"
+
+        db.query(sql, [userValues], (err, result) => {
+
+            if (err) return res.json({ Error: "Error insertando datos en el servidor" });
+
+            const ID_Usuario = result.insertId
+            const clientValues = [ID_Usuario, Documento, Nombre, Apellidos, Telefono];
+
+            const sqlInsertClient = "INSERT INTO cliente (`ID_Usuario`, `Documento`, `Nombre`, `Apellido`, `Telefono`) VALUES (?)"
+
+            db.query(sqlInsertClient, [clientValues], (err, clienteResult) => {
+
+                if (err) return res.json({ Error: "Error insertando datos en el servidor" });
+
+                return res.json({ Status: "Success" });
+
+            });
+        });
+    });
+};
+
+module.exports = { postRegistro }
