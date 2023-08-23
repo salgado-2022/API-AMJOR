dbf8546014937c56a21e42df5a1b69bbe31e4abd
juandaxx2343 <juandathebass@gmail.com>
2023-08-23 16:52:17 -0500

    arreglos para lista de usuarios

--- controllers/Usuarios/listarUsuario.js
+++ controllers/Usuarios/listarUsuario.js
@@ -4,12 +4,10 @@ const listarUsuario = (req, res) => {
   // Consulta SQL para obtener todos los usuarios con su rol, incluyendo activos e inactivos
   const sql = `
   SELECT U.idUsuario, U.correo, U.Estado, R.Nombre_Rol, C.ID_Cliente, C.Documento, C.Nombre
-  FROM usuario U
-  LEFT JOIN rol R ON U.ID_Rol = R.ID_Rol
-  LEFT JOIN cliente C ON U.idUsuario = C.ID_Cliente
-  ORDER BY U.idUsuario ASC;
-  
-  
+FROM usuario U
+LEFT JOIN rol R ON U.ID_Rol = R.ID_Rol
+LEFT JOIN cliente C ON U.idUsuario = C.ID_Usuario 
+ORDER BY U.idUsuario ASC;
   `;
 
   // Ejecuta la consulta en la base de datos
