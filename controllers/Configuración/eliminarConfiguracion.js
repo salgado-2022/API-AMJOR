0d5536a24f93ba1e9045bf7c10ee7b7be6096cc1
juandaxx2343 <juandathebass@gmail.com>
2023-08-29 18:20:26 -0500

    crear el rol con los permisos db

--- controllers/Configuración/eliminarConfiguracion.js
+++ controllers/Configuración/eliminarConfiguracion.js
@@ -1,17 +1,31 @@
 const db = require("../../database/db");
 
-const eliminarRol = ( '/delete/:id', (req, res) =>{
-  const sql = "DELETE FROM rol WHERE ID_Rol = ?";
+const eliminarRol = (req, res) => {
   const id = req.params.id;
 
-  db.query(sql, [id], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.json({ Message: "Error en el servidor" });
-    }
+  if (!id) {
+    return res.status(400).json({ error: "ID de rol no proporcionado" });
+  }
 
-    return res.json(result);
-  });
-});
+  const sql = "DELETE FROM rol WHERE ID_Rol = ?";
+  
+  try {
+    db.query(sql, [id], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Error en el servidor al eliminar el rol" });
+      }
+
+      if (result.affectedRows === 0) {
+        return res.status(404).json({ error: "No se encontró el rol a eliminar" });
+      }
+
+      return res.json({ message: "Rol eliminado exitosamente" });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error en el servidor al procesar la solicitud" });
+  }
+};
 
 module.exports = { eliminarRol };
--- controllers/Usuarios/listarUsuario.js
+++ controllers/Usuarios/listarUsuario.js
@@ -3,7 +3,7 @@ const db = require("../../database/db");
 const listarUsuario = (req, res) => {
   // Consulta SQL para obtener todos los usuarios con su rol, incluyendo activos e inactivos
   const sql = `
-  SELECT U.idUsuario, U.correo, U.Estado, R.Nombre_Rol, C.ID_Cliente, C.Documento, C.Nombre
+  SELECT U.idUsuario, U.correo, U.Estado, R.Nombre_Rol, C.ID_Cliente, C.Documento, C.Nombre, C.Apellido
 FROM usuario U
 LEFT JOIN rol R ON U.ID_Rol = R.ID_Rol
 LEFT JOIN cliente C ON U.idUsuario = C.ID_Usuario 
