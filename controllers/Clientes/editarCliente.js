0932242be73102fc532d440165d133ff9e8f726f
juandaxx2343 <juandathebass@gmail.com>
2023-09-11 00:08:41 -0500

    editar de cliente y usuarios terminado

--- controllers/Clientes/editarCliente.js
+++ controllers/Clientes/editarCliente.js
@@ -1,50 +1,86 @@
-const db = require('../../database/db');
-const bcrypt = require('bcrypt');
+const db = require("../../database/db");
+const bcrypt = require("bcrypt");
 
-// Función para editar un cliente por su ID
-const editarCliente = (req, res) => {
-  const { id } = req.params;
-  const { documento, nombre, apellido, telefono, estado } = req.body;
-
-  // Crea la consulta SQL para actualizar el cliente
+const listarEdUsuarios = (req, res) => {
+  const id = req.params.id;
   const sql = `
-    UPDATE cliente
-    SET Documento = ?, Nombre = ?, Apellido = ?, Telefono = ?
-    WHERE ID_Cliente = ?;
+    SELECT u.*, c.ID_Cliente, c.Nombre AS Nombre_Cliente, c.Documento AS Documento_Cliente, 
+    c.Telefono AS Telefono_Cliente, c.Apellido AS Apellido_Cliente
+    FROM usuario u
+    INNER JOIN cliente c ON c.ID_Usuario = u.idUsuario
+    WHERE u.idUsuario = ?;
   `;
-
-  const values = [documento, nombre, apellido, telefono, id];
-
-  // Ejecuta la consulta SQL para actualizar el cliente
-  db.query(sql, values, (err, result) => {
+  
+  db.query(sql, [id], (err, result) => {
     if (err) {
-      console.error(err);
-      return res.status(500).json({ Message: 'Error en el servidor' });
+      console.log(err);
+      return res.status(500).json({ Message: "Error en el servidor" });
     }
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ Message: 'Cliente no encontrado' });
-    }
-
-    // Luego, crea la consulta SQL para actualizar el usuario (estado)
-    const usuarioSql = `
-      UPDATE usuario
-      SET Estado = ?
-      WHERE idUsuario = ?;
-    `;
-
-    const usuarioValues = [estado, id];
-
-    // Ejecuta la consulta SQL para actualizar el usuario
-    db.query(usuarioSql, usuarioValues, (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ Message: 'Error en el servidor' });
-      }
-
-      return res.json({ Message: 'Cliente y usuario actualizados exitosamente' });
-    });
+    return res.json(result[0]);  // Retorna el primer resultado (debería ser único por ID)
   });
 };
 
-module.exports = { editarCliente };
+const editarCliente = (req, res) => {
+  const id = req.params.id;
+  const nuevoDocumento = req.body.documento;
+  const nuevoApellido = req.body.apellido;
+  const nuevoTelefono = req.body.telefono;
+  const nuevoNombre = req.body.nombre;
+  const nuevoCorreo = req.body.correo;
+  let nuevaContrasena = req.body.contrasena;
+  const nuevoEstado = req.body.estado;
+
+if (!nuevaContrasena) {
+   ActualizarUsuario = "UPDATE usuario SET correo = ?, Estado = ? WHERE idUsuario = ?";
+  values = [ nuevoCorreo, nuevoEstado, id ]
+} else {
+   ActualizarUsuario = "UPDATE usuario SET correo = ?, contrasena = ?, Estado = ? WHERE idUsuario = ?";
+  bcrypt.hash(nuevaContrasena, 10, (err, hash) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ Message: "Error en el servidor" });
+    }
+    nuevaContrasena = hash;
+
+  }); 
+  values = [ nuevoCorreo,nuevaContrasena, nuevoEstado, id ]
+}
+
+db.query(ActualizarUsuario, values, (err, result) => {
+
+  if (err) {
+    console.log(err);
+    return res.status(500).json({ Message: "Error en el servidor" });
+  }
+  const ActualizarClientes = "UPDATE cliente SET Documento = ?, Nombre = ?, Apellido = ?, Telefono = ? WHERE ID_Usuario = ?";
+
+db.query(ActualizarClientes, [ nuevoDocumento,nuevoNombre,nuevoApellido, nuevoTelefono, id ], (err, result) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).json({ Message: "Error en el servidor" });
+  }
+  });
+});
+
+ // Después de la actualización, obtener los datos actualizados
+ const sql = `
+ SELECT u.*, c.ID_Cliente, c.Nombre AS Nombre_Cliente, c.Documento AS Documento_Cliente, 
+ c.Telefono AS Telefono_Cliente, c.Apellido AS Apellido_Cliente
+ FROM usuario u
+ INNER JOIN cliente c ON c.ID_Usuario = u.idUsuario
+ WHERE u.idUsuario = ?;
+`;
+
+db.query(sql, [id], (err, result) => {
+
+ if (err) {
+   console.log(err);
+   return res.status(500).json({ Message: "Error en el servidor" });
+ }
+ 
+ return res.json(result[0]);  // Retorna los datos actualizados
+});
+
+};
+
+module.exports = { listarEdUsuarios, editarCliente };
--- controllers/Usuarios/editarUsuario.js
+++ controllers/Usuarios/editarUsuario.js
@@ -32,112 +32,58 @@ const editarUsuario = (req, res) => {
   const nuevoID_Rol = req.body.ID_Rol;
   const nuevoEstado = req.body.estado;
 
-  // Consulta SQL para obtener el teléfono actual del cliente
-  const obtenerTelefonoActual = "SELECT Telefono FROM cliente WHERE ID_Cliente = ?";
-  db.query(obtenerTelefonoActual, [id], (err, resultTelefono) => {
+if (!nuevaContrasena) {
+   ActualizarUsuario = "UPDATE usuario SET ID_Rol = ?, correo = ?, Estado = ? WHERE idUsuario = ?";
+  values = [ nuevoID_Rol,nuevoCorreo, nuevoEstado, id ]
+} else {
+   ActualizarUsuario = "UPDATE usuario SET ID_Rol = ?, correo = ?, contrasena = ?, Estado = ? WHERE idUsuario = ?";
+  bcrypt.hash(nuevaContrasena, 10, (err, hash) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ Message: "Error en el servidor" });
     }
-    const sqlCliente = "UPDATE cliente SET Documento = ?, Nombre = ?, Apellido = ?, Telefono = ? WHERE ID_Cliente = ?";
-    const valuesCliente = [nuevoDocumento, nuevoNombre, nuevoApellido, nuevoTelefono, id];
+    nuevaContrasena = hash;
 
-    db.query(sqlCliente, valuesCliente, (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ Message: "Error en el servidor" });
-      }
+  }); 
+  values = [ nuevoID_Rol,nuevoCorreo,nuevaContrasena, nuevoEstado, id ]
+}
 
-      const obtenerContrasenaActual = "SELECT contrasena FROM usuario WHERE idUsuario = ?";
-      db.query(obtenerContrasenaActual, [id], (err, result) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).json({ Message: "Error en el servidor" });
-        }
+db.query(ActualizarUsuario, values, (err, result) => {
 
-        const contrasenaActual = result[0].contrasena;
+  if (err) {
+    console.log(err);
+    return res.status(500).json({ Message: "Error en el servidor" });
+  }
+  const ActualizarClientes = "UPDATE cliente SET Documento = ?, Nombre = ?, Apellido = ?, Telefono = ? WHERE ID_Usuario = ?";
 
-        if (!nuevaContrasena) {
-          // Si no ingresa cambios para una nueva contraseña, mantener la contraseña actual
-          nuevaContrasena = contrasenaActual;
-          ejecutarActualizacion();
-        } else {
-          // Si se ingresa una nueva contraseña, encriptarla y luego realizar la actualización
-          bcrypt.hash(nuevaContrasena, 10, (err, hash) => {
-            if (err) {
-              console.log(err);
-              return res.status(500).json({ Message: "Error en el servidor" });
-            }
-            nuevaContrasena = hash;
-            ejecutarActualizacion();
-          });
-        }
-      });
-
-      const ejecutarActualizacion = () => {
-        const actualizarCamposUsuario = [];
-        if (nuevoCorreo) {
-          actualizarCamposUsuario.push("correo = ?");
-        }
-        if (nuevaContrasena) {
-          actualizarCamposUsuario.push("contrasena = ?");
-        }
-        if (nuevoID_Rol !== undefined) {
-          actualizarCamposUsuario.push("ID_Rol = ?");
-        }
-        if (nuevoEstado !== undefined) {
-          actualizarCamposUsuario.push("Estado = ?");
-        }
-        const valuesUsuario = [nuevoCorreo, nuevaContrasena, nuevoID_Rol, nuevoEstado, id];
-        const sqlUsuario = "UPDATE usuario SET " + actualizarCamposUsuario.join(', ') + " WHERE idUsuario = ?";
-
-        // Verificar si se proporcionó un nuevo teléfono o mantener el actual
-        if (req.body.telefono !== undefined) {
-          const nuevoTelefono = req.body.telefono;
-          const sqlTelefono = "UPDATE cliente SET Telefono = ? WHERE ID_Cliente = ?";
-          const valuesTelefono = [nuevoTelefono, id];
-
-          db.query(sqlTelefono, valuesTelefono, (err, resultTelefono) => {
-            if (err) {
-              console.log(err);
-              return res.status(500).json({ Message: "Error en el servidor" });
-            }
-            ejecutarActualizacionUsuario();
-          });
-        } else {
-          ejecutarActualizacionUsuario();
-        }
-
-        const ejecutarActualizacionUsuario = () => {
-          db.query(sqlUsuario, valuesUsuario, (err, result) => {
-            if (err) {
-              console.log(err);
-              return res.status(500).json({ Message: "Error en el servidor" });
-            }
-            
-            // Después de la actualización, obtener los datos actualizados
-            const sql = `
-              SELECT u.*, r.ID_Rol, c.ID_Cliente, c.Nombre AS Nombre_Cliente, c.Documento AS Documento_Cliente, 
-              c.Telefono AS Telefono_Cliente, c.Apellido AS Apellido_Cliente
-              FROM usuario u
-              INNER JOIN rol r ON u.ID_Rol = r.ID_Rol
-              INNER JOIN cliente c ON c.ID_Usuario = u.idUsuario
-              WHERE u.idUsuario = ?;
-            `;
-
-            db.query(sql, [id], (err, result) => {
-              if (err) {
-                console.log(err);
-                return res.status(500).json({ Message: "Error en el servidor" });
-              }
-              
-              return res.json(result[0]);  // Retorna los datos actualizados
-            });
-          });
-        };
-      };
-    });
+db.query(ActualizarClientes, [ nuevoDocumento,nuevoNombre,nuevoApellido, nuevoTelefono, id ], (err, result) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).json({ Message: "Error en el servidor" });
+  }
   });
+});
+
+ // Después de la actualización, obtener los datos actualizados
+ const sql = `
+ SELECT u.*, r.ID_Rol, c.ID_Cliente, c.Nombre AS Nombre_Cliente, c.Documento AS Documento_Cliente, 
+ c.Telefono AS Telefono_Cliente, c.Apellido AS Apellido_Cliente
+ FROM usuario u
+ INNER JOIN rol r ON u.ID_Rol = r.ID_Rol
+ INNER JOIN cliente c ON c.ID_Usuario = u.idUsuario
+ WHERE u.idUsuario = ?;
+`;
+
+db.query(sql, [id], (err, result) => {
+
+ if (err) {
+   console.log(err);
+   return res.status(500).json({ Message: "Error en el servidor" });
+ }
+ 
+ return res.json(result[0]);  // Retorna los datos actualizados
+});
+
 };
 
 module.exports = { listarEdUsuarios, editarUsuario };
